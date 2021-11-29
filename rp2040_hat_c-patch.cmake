031d35743b77df7cffed5f1def50891d9f4741b2
hyoyun-Kim <hyo-yun1103@daum.net>
2021-11-29 15:37:25 +0900

    add ftp_client patch

--- rp2040_hat_c-patch.cmake
+++ rp2040_hat_c-patch.cmake
@@ -12,7 +12,6 @@ message("RP2040-HAT-C patch utils found")
 
 set(RP2040_HAT_C_SRC_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
 set(IOLIBRARY_DRIVER_SRC_DIR "${RP2040_HAT_C_SRC_DIR}/libraries/ioLibrary_Driver")
-#set(MBEDTLS_SRC_DIR "${RP2040_HAT_C_SRC_DIR}/libraries/mbedtls")
 set(RP2040_HAT_C_PATCH_DIR "${RP2040_HAT_C_SRC_DIR}/patches")
 
 if(EXISTS "${IOLIBRARY_DRIVER_SRC_DIR}/.git")
@@ -36,7 +35,7 @@ message("submodules ioLibrary_Driver initialised")
 
 file(GLOB IOLIBRARY_DRIVER_PATCHES 
 	"${RP2040_HAT_C_PATCH_DIR}/01_iolibrary_driver_ethernet_chip.patch" 
-	#"${RP2040_HAT_C_PATCH_DIR}/02_iolibrary_driver_ftp_client.patch"
+	"${RP2040_HAT_C_PATCH_DIR}/02_iolibrary_driver_ftp_client.patch"
 	)
 
 foreach(IOLIBRARY_DRIVER_PATCH IN LISTS IOLIBRARY_DRIVER_PATCHES)
@@ -46,18 +45,3 @@ foreach(IOLIBRARY_DRIVER_PATCH IN LISTS IOLIBRARY_DRIVER_PATCHES)
 		WORKING_DIRECTORY ${IOLIBRARY_DRIVER_SRC_DIR}
 	)
 endforeach()
-
-# mbedtls patch
-# message("submodules mbedtls initialised")
-
-# file(GLOB MBEDTLS_PATCHES 
-# 	"${RP2040_HAT_C_PATCH_DIR}/03_mbedtls_test_mode.patch"
-# 	)
-
-# foreach(MBEDTLS_PATCH IN LISTS MBEDTLS_PATCHES)
-# 	message("Running patch ${MBEDTLS_PATCH}")
-# 	execute_process(
-# 		COMMAND ${GIT_EXECUTABLE} apply --ignore-whitespace ${MBEDTLS_PATCH}
-# 		WORKING_DIRECTORY ${MBEDTLS_SRC_DIR}
-# 	)
-# endforeach()
